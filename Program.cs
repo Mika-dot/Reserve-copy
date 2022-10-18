b5e4f7a545cf710fb0a78a3560d2ae6e420a3a8e
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2022-10-18 22:02:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using CopyFile;
+using System.IO;
+
+
+CopyFileD.CopyDelete(
+    @"Путь что копируем",
+    @"Путь куда копируем"
+    );
+
+CopyFileD.CopyOverwriting(
+    @"Путь что копируем",
+    @"Путь куда копируем"
+    );
