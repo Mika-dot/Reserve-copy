cfee28067be74b7bb22a9613c06e8bee528f76c0
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2022-10-18 20:22:19 +0300

    Add files via upload

---
+++ FileMD5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Md5
+{
+    internal class FileMD5
+    {
+        public static string Start(string path)
+        {
+            using (FileStream fs = System.IO.File.OpenRead(path))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] fileData = new byte[fs.Length];
+                fs.Read(fileData, 0, (int)fs.Length);
+                byte[] checkSum = md5.ComputeHash(fileData);
+                string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                return result;
+            }
+        }
+        public static string Start2(string path)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(path))
+                {
+                    string result = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", String.Empty);
+                    return result;
+                }
+            }
+        }
+    }
+}
