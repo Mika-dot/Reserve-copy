87d5d64c6f22c8138240dd7e1ed613b5f2f73cf4
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2022-10-18 21:04:34 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿using Md5;
 
-string thisFileMD5 = FileMD5.Start2(@"C:\Users\akimp\OneDrive\Рабочий стол\Cad-OpenGL — копия\OpenGL_lesson_CSharp\OpenGL_lesson_CSharp\SharpGLForm.cs");
+string thisFileMD5 = FileMD5.LongFile(@"Путь");
 Console.WriteLine("md5 : " + thisFileMD5);
